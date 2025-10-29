819fcf85b23abe1651752ef469effe61ab9dbcff
GiriPrasath.D <hapisnake@gmail.com>
2025-10-29 09:47:10 +0530

    Add API call to fetch and display watermelon data
    Fetch watermelon data from Smoothiefroot API and display it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,8 @@ if ingredients_list:
             st.error(f"❌ Something went wrong: {e}")
 else:
     st.info("Please select up to 5 ingredients to create your smoothie.")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+sf_df st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
